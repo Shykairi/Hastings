a1b69640876c69cd163b5ffff91b17909babf04e
Fernando Barreto <152815498+FeBarretos@users.noreply.github.com>
2025-03-03 17:41:23 -0300

    Primeira entrega(estilizar)
    Estilizar a primeira entrega e fazer as alterações conforma necessário até o dia 05

--- PI_3_Defensores_de_Hastings/PI_3_Defensores_de_Hastings/Form1.Designer.cs
+++ PI_3_Defensores_de_Hastings/PI_3_Defensores_de_Hastings/Form1.Designer.cs
@@ -28,34 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnPartida = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nomeDaPartida = new System.Windows.Forms.TextBox();
+            this.senhaDaPartida = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nomeDoGrupo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idDaPartida = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // btnPartida
+            // 
+            this.btnPartida.Location = new System.Drawing.Point(152, 92);
+            this.btnPartida.Name = "btnPartida";
+            this.btnPartida.Size = new System.Drawing.Size(75, 23);
+            this.btnPartida.TabIndex = 0;
+            this.btnPartida.Text = "Criar";
+            this.btnPartida.UseVisualStyleBackColor = true;
+            this.btnPartida.Click += new System.EventHandler(this.btnPartida_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Partida:";
+            // 
+            // nomeDaPartida
+            // 
+            this.nomeDaPartida.Location = new System.Drawing.Point(12, 52);
+            this.nomeDaPartida.Name = "nomeDaPartida";
+            this.nomeDaPartida.Size = new System.Drawing.Size(100, 20);
+            this.nomeDaPartida.TabIndex = 2;
+            // 
+            // senhaDaPartida
+            // 
+            this.senhaDaPartida.Location = new System.Drawing.Point(140, 52);
+            this.senhaDaPartida.Name = "senhaDaPartida";
+            this.senhaDaPartida.Size = new System.Drawing.Size(100, 20);
+            this.senhaDaPartida.TabIndex = 4;
+            this.senhaDaPartida.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Senha:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nomeDoGrupo
+            // 
+            this.nomeDoGrupo.Location = new System.Drawing.Point(273, 52);
+            this.nomeDoGrupo.Name = "nomeDoGrupo";
+            this.nomeDoGrupo.Size = new System.Drawing.Size(100, 20);
+            this.nomeDoGrupo.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Grupo:";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleRole = System.Windows.Forms.AccessibleRole.Diagram;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Defensores de Hastings\r\n";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // idDaPartida
+            // 
+            this.idDaPartida.Location = new System.Drawing.Point(487, 52);
+            this.idDaPartida.Name = "idDaPartida";
+            this.idDaPartida.Size = new System.Drawing.Size(100, 20);
+            this.idDaPartida.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(487, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(753, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(38, 26);
+            this.button1.Location = new System.Drawing.Point(29, 187);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 36);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Buscar Partidas";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 216);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 225);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(182, 216);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(139, 225);
+            this.listBox2.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.idDaPartida);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nomeDoGrupo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.senhaDaPartida);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nomeDaPartida);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPartida);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lobby";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button btnPartida;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nomeDaPartida;
+        private System.Windows.Forms.TextBox senhaDaPartida;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nomeDoGrupo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox idDaPartida;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- PI_3_Defensores_de_Hastings/PI_3_Defensores_de_Hastings/Form1.cs
+++ PI_3_Defensores_de_Hastings/PI_3_Defensores_de_Hastings/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KingMeServer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,16 +11,112 @@ using System.Windows.Forms;
 
 namespace PI_3_Defensores_de_Hastings
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            label6.Text = Jogo.versao;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            return;
+            string retorno = Jogo.ListarPartidas("T");
+            
+
+            retorno = retorno.Replace("\r", "");
+            retorno = retorno.Substring(0, retorno.Length - 1);
+            string[] partidas = retorno.Split('\n');
+
+            listBox1.Items.Clear();
+            for (int i = 0; i < partidas.Length - 1; i++)
+            {
+                listBox1.Items.Add(partidas[i]);
+            }
         }
+        private void btnPartida_Click(object sender, EventArgs e)
+        {
+            string nome = nomeDaPartida.Text;
+            string senha = senhaDaPartida.Text;
+            string grupo = nomeDoGrupo.Text;
+
+            string id = KingMeServer.Jogo.CriarPartida(nome, senha, grupo);
+            idDaPartida.Text = id;
+
+            if (nome == string.Empty)
+            {
+                MessageBox.Show("Insira o nome da partida: ");
+                return;
+            }
+
+            if (senha == string.Empty)
+            {
+                MessageBox.Show("Insira a sua senha: ");
+                return;
+            }
+
+            if (grupo == string.Empty)
+            {
+                MessageBox.Show("Insira o nome do grupo: ");
+                return;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string partida = listBox1.SelectedItem.ToString();
+            string[] dadosDaPartida = partida.Split(',');
+
+            int idPartida = Convert.ToInt32(dadosDaPartida[0]);
+            string nomeDaPartida = dadosDaPartida[1];
+            string dataDaPartida = dadosDaPartida[2];
+
+            string retorno = Jogo.ListarJogadores(idPartida);
+            retorno = retorno.Replace("\r", "");
+            string[] jogadores = retorno.Split('\n');
+
+            listBox2.Items.Clear();
+            for (int i = 0; i < jogadores.Length - 1; i++)
+            {
+                listBox2.Items.Add(jogadores[i]);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
