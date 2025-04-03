1d696e4c6a7852344fd9f04f14b26d6a6d7d6ef7
William Takahashi <williamtakahashikeiti@gmail.com>
2025-04-03 15:37:17 -0300

    Mapa
    aparece os personagens no mapa

---
+++ PI_3_Defensores_de_Hastings/PI_3_Defensores_de_Hastings/Funções.cs
@@ -0,0 +1,17 @@
+﻿using KingMeServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_3_Defensores_de_Hastings
+{
+    class Funções
+    {
+       
+
+
+
+    }
+}
